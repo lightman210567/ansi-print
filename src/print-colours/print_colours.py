f135cd5746bb100b5a14a258a6b835983d34ea60
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2023-09-30 21:50:51 +0100

    added foreground colours to the colours class
    added the ansi codes for foreground colours to the new colours class
    
    new file: src/print-colours/__init__.py
    new file: src/print-colours/__init__.py
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

---
+++ src/print-colours/print_colours.py
@@ -0,0 +1,9 @@
+class colours:
+    fgBlack = "\033[0;30m"
+    fgRed = "\033[0;31m"
+    fgGreen = "\033[0;32m"
+    fgYellow = "\033[0;33m"
+    fgBlue = "\033[0;34m"
+    fgPurple = "\033[0;35m"
+    fgCyan = "\033[0;36m"
+    fgWhite = "\033[0;37m"
